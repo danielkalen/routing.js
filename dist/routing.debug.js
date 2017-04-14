314baf7904d27a23f93a14558658d15c068c16ac
danielkalen <d@danielkalen.com>
2017-04-14 17:27:45 -0400

    [Build]

--- dist/routing.debug.js
+++ dist/routing.debug.js
@@ -380,7 +380,7 @@
           routers.push(routerInstance = new Router(timeout));
           return routerInstance;
         };
-        this.version = '1.0.1';
+        this.version = '1.0.2';
         return this;
       };
 
